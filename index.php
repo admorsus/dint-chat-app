a01a84296300c768e28c1c98e8fd1f6e71694ecf
Miguel B <admorsus3@gmail.com>
2020-11-16 10:06:33 +0100

    primer commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+error_reporting(E_ALL);				// directivas para activar 
+ini_set('display_errors', '1');	// la notificación de errores
+
+session_start();
+
+include_once 'model/bd.inc.php';
+
+include_once 'view/header.inc.php';
+include_once 'view/footer.inc.php';
+
+include_once 'controller/controller.inc.php';
+
+include_once 'view/show_view.php';
+
+actualizar_sesion();					// tareas relacionadas con la sesión
+
+show_header();
+show_menu();
+show_content();
+show_footer();
+
+
+?>
